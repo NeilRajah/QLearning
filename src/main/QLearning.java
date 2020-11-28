d682284aba271913139bcf56640a7a13ca6007b4
NeilRajah <neil.balaskandarajah@gmail.com>
2020-11-28 01:09:43 -0500

    Training Next
    Can create Environment from file

--- src/main/QLearning.java
+++ src/main/QLearning.java
@@ -9,12 +9,50 @@ package main;
 
 public class QLearning {
 
+	/**
+	 * Run the simulation
+	 */
 	public static void main(String[] args) {
-		int width = 5;
-		int height = 5;
-		int[] start = new int[] {0,0};
+		int width = 8;
+		int height = 8;
 		int[] goal = new int[] {width-1, height-1};
-		int[][] obstacles = new int[width][height];
+		int numObstacles = 5;
+		int[][] obstacles = new int[numObstacles][];
+		for (int o = 0; o < numObstacles; o++)
+			obstacles[o] = new int[] {(int) (Math.random() * (width-1)), (int) (Math.random() * (height-1))};
 		
+//		Environment env = new Environment(width, height, goal, obstacles);
+//		int[][] rewards = env.getRewards();
+//		print2DArray(rewards);
+		
+		Environment env = new Environment("src/main/basic.env");
+		print2DArray(env.getRewards(), "%4d ");
+	}
+	
+	/**
+	 * Print a 2D array 
+	 * @param arr Array to print
+	 * @param formatStr Format for output of each element
+	 */
+	public static void print2DArray(int[][] arr, String formatStr) {
+		for (int x = 0; x < arr.length; x++) {
+			for (int y = 0; y < arr[x].length; y++) {
+				System.out.printf(formatStr, arr[x][y]);
+			}
+			System.out.println();
+		}
+	}
+	
+	/**
+	 * Print a 2D array 
+	 * @param arr Array to print
+	 */
+	public static void print2DArray(int[][] arr) {
+		for (int x = 0; x < arr.length; x++) {
+			for (int y = 0; y < arr[x].length; y++) {
+				System.out.printf("%d ", arr[x][y]);
+			}
+			System.out.println();
+		}
 	}
 }
